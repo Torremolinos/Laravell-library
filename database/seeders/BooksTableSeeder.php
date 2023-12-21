c542c28f6fbae8a96af33bd9c3fa1669b5e43557
Torremolinos <adrian_2054@hotmail.com>
2023-12-21 09:14:45 +0100

    several changes

--- database/seeders/BooksTableSeeder.php
+++ database/seeders/BooksTableSeeder.php
@@ -13,9 +13,14 @@ class BooksTableSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('books')->insert([
+        DB::table('books') -> insert([
             'title' => 'El gran Gatsby',
             'published_year' => 1925
+        ]);
+
+        DB::table('books') -> insert([
+            'title' => 'Cien años de soledad',
+            'published_year' => 1967
         ]);
     }
 }
