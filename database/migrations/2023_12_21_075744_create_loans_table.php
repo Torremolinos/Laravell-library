45be0b0ee4e8212b36de2ea864d69c03367f79e8
Torremolinos <adrian_2054@hotmail.com>
2023-12-21 09:03:53 +0100

    migrates and seed

--- database/migrations/2023_12_21_075744_create_loans_table.php
+++ database/migrations/2023_12_21_075744_create_loans_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->date('loan_date');
+            $table->date('return_date');
         });
     }
 
